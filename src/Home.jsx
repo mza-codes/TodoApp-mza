c593fe39c10950816d3fa3acdf6f0092c38b179d
muzammil <muzofficial90@gmail.com>
2022-11-06 16:26:27 +0530

    Minor Changes

--- src/Home.jsx
+++ src/Home.jsx
@@ -3,14 +3,19 @@ import './App.css';
 import { timeGreeting } from './Utils/TimeUtils';
 import './Components/leftSection.css';
 import './Components/rightSection.css';
+import './Components/centerSection.css';
 function Home() {
     const today = new Date().toLocaleString("en-IN", { weekday: "long", });
     const [err, setErr] = useState(false);
     const time = timeGreeting[new Date().getHours()];
     const [text, setText] = useState("");
+    const [desc, setDesc] = useState("");
     const [todos, setTodos] = useState([]);
     const [completedTodos, setCompletedTodos] = useState([]);
     const [deleted, setDeleted] = useState([]);
+    const [userTodos, setUserTodos] = useState([]);
+
+    console.log('USERTODOS', userTodos);
 
     const regEx = /^[a-zA-Z][a-zA-Z ]*$/;
 
@@ -20,21 +25,35 @@ function Home() {
             setErr(true);
             return;
         } else {
-            setTodos((curr) => ([...curr, text]));
+            const todoItem = {
+                id: Date.now(),
+                deleted: false,
+                completed: false,
+                pending: true,
+                description: desc,
+                task: text
+            };
+            console.log("Cooked Value", todoItem);
+            setTodos((curr) => ([...curr, todoItem]));
             setText("");
             setErr(false);
+            setDesc("");
             // localStorage.setItem("mza-TodoApp", JSON.stringify(todos));
             return;
         };
     };
 
     const handleDelete = (data) => {
-        setTodos((curr) => ([...curr.filter(item => item !== data)]));
+        setTodos((curr) => ([...curr.filter(item => item.id !== data.id)]));
+        data.deleted = true;
+        data.pending = false;
         setDeleted((current) => ([...current, data]));
     };
 
     const handleDone = (data) => {
-        setTodos((curr) => ([...curr.filter(item => item !== data)]));
+        setTodos((curr) => ([...curr.filter(item => item.id !== data.id)]));
+        data.completed = true;
+        data.pending = false;
         setCompletedTodos((curr) => ([...curr, data]));
     };
 
@@ -43,6 +62,26 @@ function Home() {
         if (data !== null || undefined) {
             setTodos(JSON.parse(data));
         };
+    }, []);
+
+    // useEffect(() => {
+
+    // }, []);
+
+    window.onbeforeunload = () => {
+        console.log("saving");
+        const userData = {
+            todos: todos, completedTodos: completedTodos, deleted: deleted
+        };
+        setUserTodos((current) => ([...current, userData]));
+        localStorage.setItem("mza-TodoApp", JSON.stringify(userData));
+        console.log("saved DATA");
+        return "Session will be lost";
+    };
+
+    useEffect(() => {
+        console.log('SETTING TO USER ARRAY');
+
     }, []);
 
     return (
@@ -54,7 +93,7 @@ function Home() {
                         <div className="todoContainer">
                             {todos?.map((todo, i) => (
                                 <div className="todoItem" key={i}>
-                                    {todo.substring(0, 18)}
+                                    {todo.task.substring(0, 18)}
                                     <div className='doneBtn' onClick={e => handleDone(todo)}>
                                         <i className="bi bi-list-check"></i>
                                     </div>
@@ -65,7 +104,7 @@ function Home() {
                             ))}
                             {deleted?.map((todo, i) => (
                                 <div className={`doneItem ${"deleted"}`} key={i}>
-                                    {todo.substring(0, 18)}
+                                    {todo.task.substring(0, 18)}
                                     <div className="deletedBtn" onClick={e => console.log(e)}>
                                         <i className="bi bi-file-earmark-x-fill"></i>
                                     </div>
@@ -76,17 +115,25 @@ function Home() {
                 }
             </div>
             <div className='card'>
-                <div className="addData">
-                    <h2>What's Up!</h2>
-                    <p>It's {today} {time} & You're still Up! <br />
-                        How about creating a to do list!</p>
-                    <input type="text" maxLength="50" name='todo' className={`textInput ${err ? "showErr" : ""}`} id='textInput'
-                        onKeyPress={e => { if (e.key === "Enter") { setText(e.target.value); handleList() } }}
-                        value={text} placeholder="Workout..."
-                        onChange={e => setText(e.target.value)} />
-                    <div className='addBtn' onClick={handleList}><i className="bi bi-file-plus"></i></div>
+                <div className="centerWrapper">
+                    <div className="addData">
+                        <h2>What's Up!</h2>
+                        <p>It's {today} {time} & You're still Up! <br />
+                            How about creating a to do list!</p>
+                        <input type="text" maxLength="50" name='todo' className={`textInput ${err ? "showErr" : ""}`} id='textInput'
+                            onKeyPress={e => { if (e.key === "Enter") { setText(e.target.value); handleList() } }}
+                            value={text} placeholder="Workout..."
+                            onChange={e => setText(e.target.value)} />
+                        <div className='addBtn' onClick={handleList}><i className="bi bi-file-plus"></i></div>
+                    </div>
+                    <div className="section">
+                        <p>Description</p>
+                        <textarea placeholder='Go for shopping...' value={desc} onChange={e => setDesc(e.target.value)}
+                            onKeyPress={e => { if (e.key === "Enter") { setDesc(e.target.value); handleList() } }} ></textarea>
+                        {/* <iconify-icon icon="cil:truck" /> */}
+                        <button onClick={e => { handleList(); setDesc(""); }}>Submit</button>
+                    </div>
                 </div>
-
             </div>
             <div className='card'>
                 {completedTodos.length !== 0 &&
@@ -95,7 +142,7 @@ function Home() {
                         <div className="completedContainer">
                             {completedTodos?.map((todo, i) => (
                                 <div className="doneItem" key={i}>
-                                    {todo.substring(0, 18)}
+                                    {todo.task.substring(0, 18)}
                                     <div className='completeBtn' onClick={e => console.log(e)}>
                                         <i className="bi bi-check2-all"></i>
                                     </div>
